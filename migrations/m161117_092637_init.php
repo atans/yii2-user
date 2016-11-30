d80ce0752277be99b7c92c6f62d3651ca81b3887
Atans Chiu <atans.chiu@gmail.com>
2016-11-30 14:39:02 +0800

    Save chages

--- migrations/m161117_092637_init.php
+++ migrations/m161117_092637_init.php
@@ -12,7 +12,7 @@ class m161117_092637_init extends \atans\user\migrations\Migration
             'password_hash' => $this->string(255)->notNull(),
             'password_reset_token' => $this->string(255)->unique(),
             'email' => $this->string(255)->notNull()->unique(),
-            'registration_ip' => $this->string(42)->notNull(),
+            'registration_ip' => $this->string(42)->null(),
             'status' => $this->string(32)->notNull(),
             'created_at' => $this->dateTime()->notNull(),
             'updated_at' => $this->dateTime()->notNull(),
